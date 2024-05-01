0b1879c409e9e6c91030afa4dacd6012bdc93e09
Light_Quanta <zch22248@foxmail.com>
2024-05-01 21:51:52 +0800

    add test data

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import data from '../../data/data.json'
 
 defineProps<{ msg: string }>()
 
 const count = ref(0)
+const num = data["114"]
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
+    <p>test num is {{ num }}</p>
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
       Edit
@@ -19,9 +22,8 @@ const count = ref(0)
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+    starter
   </p>
   <p>
     Install
